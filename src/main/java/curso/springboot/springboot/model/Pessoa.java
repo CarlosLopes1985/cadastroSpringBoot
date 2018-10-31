3c8b421b7b8c0be0cc0091b4a4678d32c7c7695a
caka19_rj <caka19_rj@hotmail.com>
2018-10-31 01:35:54 -0200

    Inicio Cadastro Pessoa spring boot

---
+++ src/main/java/curso/springboot/springboot/model/Pessoa.java
@@ -0,0 +1,74 @@
+package curso.springboot.springboot.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Pessoa implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer idPessoa;
+	
+	private String nome;
+	
+	private String sobreNome;
+
+	public Integer getIdPessoa() {
+		return idPessoa;
+	}
+
+	public void setIdPessoa(Integer idPessoa) {
+		this.idPessoa = idPessoa;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobreNome() {
+		return sobreNome;
+	}
+
+	public void setSobreNome(String sobreNome) {
+		this.sobreNome = sobreNome;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	@Override
+	public String toString() {
+		return "Pessoa [idPessoa=" + idPessoa + ", nome=" + nome + ", sobreNome=" + sobreNome + "]";
+	}
+
+	public Pessoa() {
+		super();
+	}
+
+	public Pessoa(Integer idPessoa, String nome, String sobreNome) {
+		super();
+		this.idPessoa = idPessoa;
+		this.nome = nome;
+		this.sobreNome = sobreNome;
+	}
+	
+	
+
+}
